6a739dd75296b681b408963e2d59d45f581e3100
Even-Lard <34374240+Even-Lard@users.noreply.github.com>
2017-12-11 21:51:52 +0800

    Update and rename echo_bot.php to config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'channelAccessToken' => '<ENorgWeG1JgAW7VqsmeYqD0rZN20Y+A++cqMWfUATyAP4gKYfqMcv5Aygg6EnHxYN2euFIkgeu2bi26KJDIKieq/Qki/yWzWZLygtpuB42wjcTweM9DnsYhaYLIcuaZkvs8vJcMPIKzm+P2GbW85wAdB04t89/1O/w1cDnyilFU=>', // Channel Access Token
+    'channelSecret' => '<026443ada574ef6fb4677cde38adfb81>' // Channel Secret
+];
+?>
