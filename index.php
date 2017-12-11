ae9a50237a245f5a25854372630031475dbd822e
Even-Lard <34374240+Even-Lard@users.noreply.github.com>
2017-12-11 21:44:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,65 @@
-<?php
- echo "R VEN";
+/* 輸入申請的Line Developers 資料  */
+	$channel_id = "{1550905925}";
+	$channel_secret = "{026443ada574ef6fb4677cde38adfb81}";
+	$channel_access_token = "{ENorgWeG1JgAW7VqsmeYqD0rZN20Y+A++cqMWfUATyAP4gKYfqMcv5Aygg6EnHxYN2euFIkgeu2bi26KJDIKieq/Qki/yWzWZLygtpuB42wjcTweM9DnsYhaYLIcuaZkvs8vJcMPIKzm+P2GbW85wAdB04t89/1O/w1cDnyilFU=}";
+
+	$myURL = "https://Your Domain/update/"
 
 
-bot.on('message', function(event) {
-  if (event.message.type = 'text') {
-    var msg = event.message.text;
-    event.reply(msg).then(function(data) {
-      // success 
-      console.log(msg);
-    }).catch(function(error) {
-      // error 
-      console.log('error');
-    });
-  }
-});
-?>
-
-
+//  當有人發送訊息給bot時 我們會收到的json
+// 	{
+// 	  "events": 
+// 	  [
+// 		  {
+// 			"replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+// 			"type": "message",
+// 			"timestamp": 1462629479859,
+// 			"source": {
+// 				 "type": "user",
+// 				 "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+// 			 },
+// 			 "message": {
+// 				 "id": "325708",
+// 				 "type": "text",
+// 				 "text": "Hello, world"
+// 			  }
+// 		  }
+// 	  ]
+// 	}
+	 
+	 
+	// 將收到的資料整理至變數
+	$receive = json_decode(file_get_contents("php://input"));
+	
+	// 讀取收到的訊息內容
+	$text = $receive->events[0]->message->text;
+	
+	// 讀取訊息來源的類型 	[user, group, room]
+	$type = $receive->events[0]->source->type;
+	
+	// 由於新版的Messaging Api可以讓Bot帳號加入多人聊天和群組當中
+	// 所以在這裡先判斷訊息的來源
+	if ($type == "room")
+	{
+		// 多人聊天 讀取房間id
+		$from = $receive->events[0]->source->roomId;
+	} 
+	else if ($type == "group")
+	{
+		// 群組 讀取群組id
+		$from = $receive->events[0]->source->groupId;
+	}
+	else
+	{
+		// 一對一聊天 讀取使用者id
+		$from = $receive->events[0]->source->userId;
+	}
+	
+	// 讀取訊息的型態 [Text, Image, Video, Audio, Location, Sticker]
+	$content_type = $receive->events[0]->message->type;
+	
+	// 準備Post回Line伺服器的資料 
+	$header = ["Content-Type: application/json", "Authorization: Bearer {" . $channel_access_token . "}"];
+	
+	// 回覆訊息
+	reply($content_type, $text);
