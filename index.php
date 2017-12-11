099d30edca3557bb2195547696ccd94de37ab725
Even-Lard <34374240+Even-Lard@users.noreply.github.com>
2017-12-11 23:13:34 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,32 @@
-<?PHP
-
-
-if (strtolower($message['text']) == "text" || $message['text'] == "文字"){
-    $client->replyMessage(array(
-        'replyToken' => $event['replyToken'],
-        'messages' => array(
-            array(
-                'type' => 'text', // 訊息類型 (文字)
-                'text' => 'Hello, world!' // 回復訊息
-            )
-        )
-    ));
-}
-
+<?php
+require_once('./LINEBotTiny.php');
+$channelAccessToken = '<your channel access token>';
+$channelSecret = '<your channel secret>';
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+foreach ($client->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+            switch ($message['type']) {
+                case 'text':
+                    $client->replyMessage(array(
+                        'replyToken' => $event['replyToken'],
+                        'messages' => array(
+                            array(
+                                'type' => 'text',
+                                'text' => $message['text']
+                            )
+                        )
+                    ));
+                    break;
+                default:
+                    error_log("Unsupporeted message type: " . $message['type']);
+                    break;
+            }
+            break;
+        default:
+            error_log("Unsupporeted event type: " . $event['type']);
+            break;
+    }
+};
 ?>
