b64e115efde775caf7835ea783be38e9104cd921
Even-Lard <34374240+Even-Lard@users.noreply.github.com>
2017-12-11 21:50:03 +0800

    Update LINEBotTiny.php

--- LINEBotTiny.php
+++ LINEBotTiny.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Copyright 2016 LINE Corporation
  *
@@ -15,7 +14,6 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-
 /*
  * This polyfill of hash_equals() is a modified edition of https://github.com/indigophp/hash-compat/tree/43a19f42093a0cd2d11874dff9d891027fc42214
  *
@@ -25,24 +23,19 @@
  */
 if (!function_exists('hash_equals')) {
     defined('USE_MB_STRING') or define('USE_MB_STRING', function_exists('mb_strlen'));
-
     function hash_equals($knownString, $userString)
     {
         $strlen = function ($string) {
             if (USE_MB_STRING) {
                 return mb_strlen($string, '8bit');
             }
-
             return strlen($string);
         };
-
         // Compare string lengths
         if (($length = $strlen($knownString)) !== $strlen($userString)) {
             return false;
         }
-
         $diff = 0;
-
         // Calculate differences
         for ($i = 0; $i < $length; $i++) {
             $diff |= ord($knownString[$i]) ^ ord($userString[$i]);
@@ -50,7 +43,6 @@ if (!function_exists('hash_equals')) {
         return $diff === 0;
     }
 }
-
 class LINEBotTiny
 {
     public function __construct($channelAccessToken, $channelSecret)
@@ -58,7 +50,6 @@ class LINEBotTiny
         $this->channelAccessToken = $channelAccessToken;
         $this->channelSecret = $channelSecret;
     }
-
     public function parseEvents()
     {
         if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
@@ -66,21 +57,17 @@ class LINEBotTiny
             error_log("Method not allowed");
             exit();
         }
-
         $entityBody = file_get_contents('php://input');
-
         if (strlen($entityBody) === 0) {
             http_response_code(400);
             error_log("Missing request body");
             exit();
         }
-
         if (!hash_equals($this->sign($entityBody), $_SERVER['HTTP_X_LINE_SIGNATURE'])) {
             http_response_code(400);
             error_log("Invalid signature value");
             exit();
         }
-
         $data = json_decode($entityBody, true);
         if (!isset($data['events'])) {
             http_response_code(400);
@@ -89,14 +76,12 @@ class LINEBotTiny
         }
         return $data['events'];
     }
-
     public function replyMessage($message)
     {
         $header = array(
             "Content-Type: application/json",
             'Authorization: Bearer ' . $this->channelAccessToken,
         );
-
         $context = stream_context_create(array(
             "http" => array(
                 "method" => "POST",
@@ -104,14 +89,12 @@ class LINEBotTiny
                 "content" => json_encode($message),
             ),
         ));
-
         $response = file_get_contents('https://api.line.me/v2/bot/message/reply', false, $context);
         if (strpos($http_response_header[0], '200') === false) {
             http_response_code(500);
-            error_log("Request failed: " . $response);
+            //error_log("Request failed: " . $response);
         }
     }
-
     private function sign($body)
     {
         $hash = hash_hmac('sha256', $body, $this->channelSecret, true);
@@ -119,3 +102,4 @@ class LINEBotTiny
         return $signature;
     }
 }
+?>
