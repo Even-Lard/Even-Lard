836185b80ed9eb1db9bd48e3ed17d860db9f8add
Even-Lard <34374240+Even-Lard@users.noreply.github.com>
2017-12-11 21:27:22 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
-<?php
- echo "R VEN";
-
-
-setTimeout(function(){
+ setTimeout(function(){
     var userId = '使用者 ID';
     var sendMsg = '要發送的文字';
     bot.push(userId,sendMsg);
     console.log('send: '+sendMsg);
 },5000);
-
-?>
